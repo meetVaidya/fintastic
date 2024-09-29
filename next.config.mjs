2205026881ff376cdb0b8fc9193c41bb7a91cba3
meetVaidya <vaidyameet2004@gmail.com>
2024-09-29 11:04:18 +0530

    Add VSCode settings and update Next.js config for Alpaca API keys

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	env: {
+		ALPACA_API_KEY_ID: process.env.ALPACA_API_KEY_ID,
+		ALPACA_API_SECRET_KEY: process.env.ALPACA_API_SECRET_KEY,
+	},
+};
+
+console.log("ALPACA_API_KEY_ID:", process.env.ALPACA_API_KEY_ID);
+console.log("ALPACA_API_SECRET_KEY:", process.env.ALPACA_API_SECRET_KEY);
 
 export default nextConfig;
